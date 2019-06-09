59f2a4d7a33f3ce255f1eeb6b40c762f496d5a95
Makiasake <alexandre.maki@hotmail.com>
2019-06-09 11:19:10 -0300

    Added class Position

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Position position = new Position(3, 5);
+		
+		System.out.println(position);
 	}
 
 }
